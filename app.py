e9911ba888e1fe34bbbacb46339a98864752913c
itsbalvant <109503483+itsbalvant@users.noreply.github.com>
2024-07-16 14:47:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Balvant! Hello, World!'
+    return 'Hello from Balvant! test!'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
